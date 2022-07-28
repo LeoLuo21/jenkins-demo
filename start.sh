8504dfcf25226e092c3b9ef93bf0b235c46ae935
leoluo <1098506605@qq.com>
2022-07-29 05:38:04 +0800

    g

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #/bin/bash
-PID=$(lsof -i :10001 | grep 10001 | awk '{print $2}')
-if [[ -n $PID ]]; then
-	kill -9 $PID
-	echo "Original Process ID - $PID has been killed."
-fi
 echo "Start compiling."
 mvn clean package spring-boot:repackage
 echo "Start executing."
