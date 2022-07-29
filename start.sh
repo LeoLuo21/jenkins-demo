9af6d36b31af9a5eed4fb3bd29996c60bae6e8be
leoluo <1098506605@qq.com>
2022-07-29 19:38:24 +0800

    p

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
-#/bin/bash
-pid=`sudo lsof -t -i:10001`
+#!/bin/bash
+pid=`lsof -t -i:10001`
 echo "pid=$pid"
 if [ -n "$pid" ]; then
-	sudo kill -9 $pid
+	kill -9 $pid
 	echo "$pid has been killed."
 fi
-echo "Start compiling."
-mvn clean package spring-boot:repackage
-echo "Start executing."
-nohup java -jar target/jenkins-demo-1.0.0.jar >/dev/null 2>&1 &
-echo "Project is running now."
