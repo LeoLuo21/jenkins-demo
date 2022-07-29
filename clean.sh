ad05679184e1626651329836913a065ff1afb26e
leoluo <1098506605@qq.com>
2022-07-29 19:49:35 +0800

    q

---
+++ clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+pid=`lsof -t -i:10001`
+echo "pid=$pid"
+if [ -n "$pid" ]; then
+	kill -9 $pid
+	echo "$pid has been killed."
+fi
--- start.sh
+++ start.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-pid=`lsof -t -i:10001`
-echo "pid=$pid"
-if [ -n "$pid" ]; then
-	kill -9 $pid
-	echo "$pid has been killed."
-fi
+#/bin/bash -xe
+nohup java -jar target/jenkins-demo-1.0.0.jar > /dev/null 2>&1 &
