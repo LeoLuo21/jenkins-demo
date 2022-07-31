8d912c2fae1dbc4b7915952ecd67cf3afb30e547
leoluo <1098506605@qq.com>
2022-07-31 17:45:03 +0800

    z

--- src/main/java/demo/OneController.java
+++ src/main/java/demo/OneController.java
@@ -12,6 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class OneController {
     @GetMapping("/")
     public String info() {
-        return "@@@SUCCESS";
+        return "SUCCESS";
     }
 }
